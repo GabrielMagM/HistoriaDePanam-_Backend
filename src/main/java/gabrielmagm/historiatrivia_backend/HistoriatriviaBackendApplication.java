cadbc5015794374a7874fe5439188f8137c35c1e
Gabriel_MagMed <gabriel.t.13@hotmail.com>
2025-10-12 22:11:11 -0500

    Inicializa proyecto Springboot de la Historia de Panamá con GitIgnore

---
+++ src/main/java/gabrielmagm/historiatrivia_backend/HistoriatriviaBackendApplication.java
@@ -0,0 +1,13 @@
+package gabrielmagm.historiatrivia_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoriatriviaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoriatriviaBackendApplication.class, args);
+	}
+
+}
